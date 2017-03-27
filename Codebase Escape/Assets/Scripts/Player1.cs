5b918b4cbeb03adc600046e39c510c6cd97d99eb
jmdfgouveia1 <jmdf.gouveia1@gmail.com>
2017-03-27 04:15:46 +0100

    Physics engine improved
    (likely not final version), also second level is now being
    developed.

--- Codebase Escape/Assets/Scripts/Player1.cs
+++ Codebase Escape/Assets/Scripts/Player1.cs
@@ -9,23 +9,31 @@ public class Player1 : MonoBehaviour {
     public bool movement;
 
     private Rigidbody2D rb;
+    private RaycastHit2D hit;
 
     void Start ()
     {
         movement = true;
         moveSpeed = 20f;
         jumpPower = 20f;
+
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1;
     }
 
 	void Update () {
+
+        hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.1f);
+
         if (movement) {
             if (Input.GetKey(KeyCode.A))
                 transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
             if (Input.GetKey(KeyCode.D))
                 transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.W))
-                rb.AddForce(Vector2.up * jumpPower);
+            if (Input.GetKeyDown(KeyCode.W) && hit.collider != null)
+                rb.AddForce(Vector2.up * jumpPower * 50);
+            if (Input.GetKeyUp(KeyCode.W))
+                rb.AddForce(Vector2.down * jumpPower * 25);
         }
     }
 
